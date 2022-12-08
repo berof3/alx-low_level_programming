927eb06af8d47ff0252f7136df48ed5ff85a269c
berof3 <eliaskassie@gmail.com>
2022-12-08 11:09:34 -0800

    C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,16 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %d byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %d byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %d byte(s)\n", sizeof(afloat));
 	return (0);
 } 
