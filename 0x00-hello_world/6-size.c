7b51c4a7ad5e2ed5c6c6dd5156d3634e5dbd7698
berof3 <eliaskassie@gmail.com>
2022-12-08 10:07:54 -0800

    C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,6 @@ int main(void)
 	long along;
 	long long alonglong;
 	float afloat;
-
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
