25d8827906626a3372b984e8d0a6a0699af383a1
berof3 <eliaskassie@gmail.com>
2022-12-10 20:51:08 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry
  *
- * Description: This program will assign a random number to the variable n each time it is executed.
+ * Description: This program will assign a random number to the variable n each time.
  *
  * Reurn: Always 0 (Success)
  */
