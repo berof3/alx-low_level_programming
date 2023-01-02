5b61ef28f5af2b0086f9d6a23c869f0dbc4d231f
berof3 <eliaskassie@gmail.com>
2023-01-02 03:54:10 -0800

    a function that copies memory area.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@
 
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
-char *_memcpy(char *dest, char *src, unsigned int n);
+void *_memcpy(void *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
